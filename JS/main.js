8726bf2db07ab69ea1d4c4b532fbc8d5ea2df36a
Hedgehog <dopierala.tb@gmail.com>
2022-11-10 11:36:23 +0100

    Add responsive Add working breadcrumb

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,9 @@ $( ".navbar__login-btn" ).on( "click", function() {
     $( ".navbar__login-btn" ).removeClass("active")
     $( this ).addClass("active")
 });
+
+
+$('.navbar__mobile-btn').on( "click", function() {
+    $('.navbar__mobile-section').toggleClass('open')
+    console.log("wtf")
+})
